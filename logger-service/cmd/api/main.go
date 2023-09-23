a3ea2f65f4c1396f0e5db53d41324844831bf5e2
KingKord <93544537+KingKord@users.noreply.github.com>
2023-09-23 18:56:55 +0300

    log-service add

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpc     = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	// connect to Mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// in some time I should read the docs on mongo-driver/mongo
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+		return nil, err
+	}
+	return c, nil
+}
